3d8439be89f4234f675e9bf9fa0d5da7619b017c
oneshi <s.yamane@newbees.jp>
2023-11-27 18:31:26 +0900

    map関数部分の分割代入を2stepに切り替え, 一部CSS修正, author部分のkey指定部分のconsoleエラーの解消

--- booklog/src/App.tsx
+++ booklog/src/App.tsx
@@ -5,7 +5,7 @@ import './App.css' //CSSはここ読み込んでね
 
 function App() {
   const [bookData, setBookData] = useState<BookItem[]>(MOCK_DATA.items); //MOCK_DATAのitemsからランダムにデータを取得し、BookItemの型配列にならって保持するstate。
-  console.log(bookData);
+
   return (
     <>
       <header className="header">
@@ -14,46 +14,51 @@ function App() {
       <main className="main">
         <div className="books-box">
           {
-            bookData.map(({ id, volumeInfo: { imageLinks, title, description, authors, publisher, previewLink } }) => (
-              <div key={id} className="books-detail">
-                {
-                  imageLinks && (
-                    <img src={imageLinks.thumbnail} className="books-img" alt={title} width="128" height="169" />
-                  )
-                }
-                <div className="books-detail-contents">
-                  <h2 className="book-ttl">{title}</h2>
+            bookData.map((book) => {
+              const { id, volumeInfo } = book;
+              const { imageLinks, title, description, authors, publisher, previewLink } = volumeInfo;
+
+              return (
+                <div key={id} className="books-detail">
                   {
-                    description && (
-                      <p className="book-description">
-                        {description}
-                      </p>
+                    imageLinks && (
+                      <img src={imageLinks.thumbnail} className="books-img" alt={title} />
                     )
                   }
-                  {
-                    authors && (
-                      <p className="book-authors-list">著者：
-                        {
-                          authors.map((author) => (
-                            <span className="author" key={id}>{author}</span>
-                          ))
-                        }
-                      </p>
-                    )
-                  }
-                  {
-                    publisher && (
-                      <p className="book-publisher">出版社：{publisher}</p>
-                    )
-                  }
-                  {
-                    previewLink && (
-                      <a href={previewLink} className="book-link">詳しく見る</a>
-                    )
-                  }
+                  <div className="books-detail-contents">
+                    <h2 className="book-ttl">{title}</h2>
+                    {
+                      description && (
+                        <p className="book-description">
+                          {description}
+                        </p>
+                      )
+                    }
+                    {
+                      authors && (
+                        <p className="book-authors-list">著者：
+                          {
+                            authors.map((author, index) => (
+                              <span className="author" key={index}>{author}</span>
+                            ))
+                          }
+                        </p>
+                      )
+                    }
+                    {
+                      publisher && (
+                        <p className="book-publisher">出版社：{publisher}</p>
+                      )
+                    }
+                    {
+                      previewLink && (
+                        <a href={previewLink} className="book-link">詳しく見る</a>
+                      )
+                    }
+                  </div>
                 </div>
-              </div>
-            ))
+              )
+            })
           }
         </div>
       </main>
